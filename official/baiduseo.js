d4434dd00397b403a7b748fc49e6ca3b4ba0130f
Iori.Xia <5663815@qq.com>
2021-12-25 14:34:08 +0800

    ~

---
+++ official/baiduseo.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement('script')
+    hm.src = 'https://hm.baidu.com/hm.js?e0da63219963c60b2aac14a9bd86f51f'
+    var s = document.getElementsByTagName('script')[0]
+    s.parentNode.insertBefore(hm, s)
+})()
